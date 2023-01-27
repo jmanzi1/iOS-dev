93ddab97e2481a974c0bd9e4b543a1c60dcb4bd2
jmanzi1 <jmanzi@nyit.edu>
2023-01-27 15:31:23 -0500

    Final prework touches

--- iOS_dev/ViewController.swift
+++ iOS_dev/ViewController.swift
@@ -14,14 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var schoolNameTextField: UITextField!
     
+    @IBOutlet weak var nationalityTextField: UITextField!
     
     @IBAction func morePetsStepper(_ sender: UIStepper) {
         numberOfPetsLabel.text = "\(Int(sender.value))"
     }
-  /*  @IBAction func stepperDidChange(_ sender: UIStepper) {
-       
-       numberOfPetsLabel.text = "\(Int(sender.value))"
-   }*/
+    
     @IBOutlet weak var numberOfPetsLabel: UILabel!
     @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
     @IBOutlet weak var morePetsStepper: UIStepper!
@@ -31,9 +29,9 @@ class ViewController: UIViewController {
         // Lets us choose the title we have selected from the segmented control
                // In our example that is whether it is first, second, third or forth
                let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
-               
+
                // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
-               let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!).I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!), a \(nationalityTextField.text!) First Gen student and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
         // Creates the alert where we pass in our message, which our introduction.
                 let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
                 
@@ -49,6 +47,8 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.lightGray
         // Do any additional setup after loading the view.
     }
 
