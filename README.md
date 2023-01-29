7463ae156a11c8d884bcff951e425e05c1aa5250
jmanzi1 <jmanzi@nyit.edu>
2023-01-28 23:00:34 -0500

    Updated readme and added a feature

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# iOS-dev
-Codepath iOS Intermediate Development: iOS Development project using Swift and Xcode
+
+Intro Application
+
+TODO:// Add app description
+The Intro App collects my information (names, nationality, school etc.) and displays it when the user clicks/taps "Introduce Self". The stepper adjusts the number of pets the user owns between 0(min) and 50(max). The switch indicates whether the user would like to have more pets or not. To dismiss the info alert box, the user taps the "Nice to meet you".
+
+App Walk-though
+TODO:// Add the URL to your animated app walk-though gif in the image tag below. Make sure the gif actually renders and animates when viewing this README. (☝️ Remove this paragraph after after adding gif). LICEcap is a great tool for recording and you can simply upload the gif to GitHub and provide the path name in place of URL as shown below.
+
+TODO:// In the Features section below, add an x between the -[ ] like this - [x] for any features you complete. (☝️ Remove this paragraph after after checking off user stories)
+
+Required Features
+1. [x] App displays an image of a school's logo
+2. [x] App has three textfields for first, last, and school names
+3. [x] App has a segmented control that changes student year
+4. [x] Number of pet matches label is increased/decreased by stepper
+5. [x] Switch makes a statement about wanting more pets or not(true/false)
+6. [x] Introduce yourself button shows alert box with an introduciton and dismiss button
+Optional Features
+1. User can tap a button to change the color of the background view
+3. [x] User can select on additional buttons that provide more info about the user. Example: more textfields, a different alert box, etc.
+4. [x] Any stylistic changes that are not default options /* I changed the background color to from white to gray */
+   
--- iOS_dev/ViewController.swift
+++ iOS_dev/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var nationalityTextField: UITextField!
     
+    @IBOutlet weak var genSegmentedControl: UISegmentedControl!
     @IBAction func morePetsStepper(_ sender: UIStepper) {
         numberOfPetsLabel.text = "\(Int(sender.value))"
     }
@@ -27,16 +28,19 @@ class ViewController: UIViewController {
     
     @IBAction func selfIntro(_ sender: UIButton) {
         // Lets us choose the title we have selected from the segmented control
-               // In our example that is whether it is first, second, third or forth
+               // In our example that is whether it is first, second, third or forth.
                let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+                //first generation or not?? Select what applies to you.
+                let gen = genSegmentedControl.titleForSegment(at: genSegmentedControl.selectedSegmentIndex)
 
                // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
-        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!), a \(nationalityTextField.text!) First Gen student and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        let introduction = "I go by \(firstNameTextField.text!) \(lastNameTextField.text!), born and raised \(nationalityTextField.text!) 🇷🇼 and I am \(gen!) college student attending \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
         // Creates the alert where we pass in our message, which our introduction.
                 let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
                 
                 // A way to dismiss the box once it pops up
-                let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+                let action = UIAlertAction(title: "Nice to meet you✨", style: .default, handler: nil)
                 
                 // Passing this action to the alert controller so it can be dismissed
                 alertController.addAction(action)
